e8fd9f68c305d6b6ba77452a6b5392530c6e2a06
William VanDolah <william.vandolah@gmail.com>
2019-01-27 17:29:15 -0600

    first

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#B2BCAA',
+
+  primary: '#121420',
+  primaryLight: '#B3B3B3',
+  primaryDark: '#7A7A7A',
+
+  secondary: '#C9CEBD',
+  secondaryLight: '#B3B3B3',
+  secondaryDark: '#121420',
+};
